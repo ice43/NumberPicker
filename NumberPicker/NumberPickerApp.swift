c560eb8cfbdc54a0c5ec997542b49fb27bae05cf
bowski43 <43mngt@gmail.com>
2024-03-12 10:42:19 +0300

    Initial Commit

---
+++ NumberPicker/NumberPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberPickerApp.swift
+//  NumberPicker
+//
+//  Created by Serge Broski on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NumberPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
